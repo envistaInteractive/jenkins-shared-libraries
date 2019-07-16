8820ec54a1ec8f615daa7fa8a5a3f94b2fecdc9e
Neil Gealy <gealyneil@gmail.com>
2019-07-16 09:37:39 -0400

    trying to fix tagging

--- vars/k8sBuildKanikoNodejs.groovy
+++ vars/k8sBuildKanikoNodejs.groovy
@@ -1,12 +1,5 @@
 def call(image) {
     sh '''#!/busybox/sh
-    /kaniko/executor --dockerfile `pwd`/Dockerfile --context `pwd` --cache=true --destination=${image}:${BUILD_NUMBER}-${GIT_COMMIT}
+    /kaniko/executor --dockerfile `pwd`/Dockerfile --context `pwd` --cache=true --destination=${image}:${BUILD_NUMBER}-${GIT_COMMIT} --destination=${image}:latest
     '''
 }
-
-
-// def call(image) {
-//     sh '''#!/busybox/sh
-//     /kaniko/executor --dockerfile `pwd`/Dockerfile --context `pwd` --cache=true --destination=${image}
-//     '''
-// }

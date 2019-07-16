f3c34f7613c6239c0e81363e232e24e0c05943f7
Neil Gealy <gealyneil@gmail.com>
2019-07-16 09:16:57 -0400

    move tagging vars to jenkins shared library

--- vars/k8sBuildKanikoNodejs.groovy
+++ vars/k8sBuildKanikoNodejs.groovy
@@ -1,5 +1,15 @@
 def call(image) {
+    image_tagged = "${image}:${env.BUILD_NUMBER}-${env.GIT_COMMIT}"
     sh '''#!/busybox/sh
-    /kaniko/executor --dockerfile `pwd`/Dockerfile --context `pwd` --cache=true --destination=${image}
+    /kaniko/executor --dockerfile `pwd`/Dockerfile --context `pwd` --cache=true --destination=${image_tagged}
     '''
 }
+
+
+// def call(image) {
+//     sh '''#!/busybox/sh
+//     /kaniko/executor --dockerfile `pwd`/Dockerfile --context `pwd` --cache=true --destination=${image}
+//     '''
+// }
+
+`
